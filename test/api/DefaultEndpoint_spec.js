b5c7f4e9b625daefdc721930afa2d36476e8d88a
Steve Wagner <ciroque@live.com>
2012-12-10 20:12:59 -0800

    updating comments on DefaultEndpoint API tests

--- test/api/DefaultEndpoint_spec.js
+++ test/api/DefaultEndpoint_spec.js
@@ -10,7 +10,7 @@ var settings_module = require('n-app-conf');
 
 var settings = new settings_module.Settings('../../lib/service/settings.json');
 
-frisby_module.create('...description...')
+frisby_module.create('Default endpoint HEAD')
     .head(
         settings.service.protocol + '://' +
         settings.service.hostname + ':' +
@@ -19,7 +19,7 @@ frisby_module.create('...description...')
     .expectHeaderContains('x-rest-ack', 'ACK')
     .toss();
 
-frisby_module.create('...description...')
+frisby_module.create('Default endpoing GET')
     .get(
         settings.service.protocol + '://' +
             settings.service.hostname + ':' +
